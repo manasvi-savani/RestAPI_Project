ae731f84b75b08cdf3bc7c79dee4678966851cd2
Wesbert <wesberte@gmail.com>
2021-08-13 16:33:10 +0000

    User Controller CLASS

--- Rest_API/src/main/java/com/citi/training/Rest_API/rest/UserController.java
+++ Rest_API/src/main/java/com/citi/training/Rest_API/rest/UserController.java
@@ -1,4 +1,22 @@
 package com.citi.training.Rest_API.rest;
 
+import com.citi.training.Rest_API.entities.User;
+import com.citi.training.Rest_API.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    Collection<User> getUsers() {
+        return userService.getAllUsers();
+    }
 }
