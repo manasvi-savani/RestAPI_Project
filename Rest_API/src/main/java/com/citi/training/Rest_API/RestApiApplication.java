9ac1644615930ad208e0170de2a0c3657b2f951d
Wesbert <wesberte@gmail.com>
2021-08-13 14:32:20 +0000

    Testing commit - Wesbert Edouard

--- Rest_API/src/main/java/com/citi/training/Rest_API/RestApiApplication.java
+++ Rest_API/src/main/java/com/citi/training/Rest_API/RestApiApplication.java
@@ -2,7 +2,7 @@ package com.citi.training.Rest_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Wesbert Commit Test
 @SpringBootApplication
 public class RestApiApplication {
 
