6e776d316841b7e17f040a849d6b88c4a163cdac
manasvi-savani <msavani0299@gmail.com>
2021-08-13 16:06:44 +0000

    sql database - insert user

--- Rest_API/sql/users_table.sql
+++ Rest_API/sql/users_table.sql
@@ -6,3 +6,8 @@ CREATE TABLE `conygre`.`users_table` (
                                          `Net_worth` DOUBLE NOT NULL,
                                          PRIMARY KEY (`id`),
                                          UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+
+INSERT INTO `conygre`.`users_table` (`id`, `Name`, `Cash_value`, `Investment_value`, `Net_worth`) VALUES ('1', 'John Smith', '1000', '500', '1500');
+INSERT INTO `conygre`.`users_table` (`id`, `Name`, `Cash_value`, `Investment_value`, `Net_worth`) VALUES ('2', 'Jane Doe', '2053', '749', '10000');
+INSERT INTO `conygre`.`users_table` (`id`, `Name`, `Cash_value`, `Investment_value`, `Net_worth`) VALUES ('3', 'Gary Snowball', '5784', '2053', '15000');
+
